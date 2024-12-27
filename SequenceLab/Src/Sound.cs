b15d89d139ae3340d1427caf677f0274d2cd11f1
yury-kozlov <glassiko@gmail.com>
2024-12-26 21:06:55 -0500

    sound - remove obsolete property

--- SequenceLab/Src/Sound.cs
+++ SequenceLab/Src/Sound.cs
@@ -1,22 +1,15 @@
-﻿using System.Text;
-
-namespace Beater;
+﻿namespace Beater;
 
 public class Sound
 {
     public Sound(string name)
     {
         Name = name;
-        Message = Encoding.ASCII.GetBytes($"{name} 1;");
     }
 
     public string Name;
     public Strategy Strategy;
     public List<Sound> Followers = new();
-
-    [Obsolete]
-    public byte[] Message;
-
 
     override public string ToString() => Name;
 }
