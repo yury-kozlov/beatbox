c3f1612eb3958c13ac774716169f1b8cbb9b14c7
yury-kozlov <glassiko@gmail.com>
2024-12-27 21:21:24 -0500

    extract techno sequence

--- SequenceLab/Src/BeatBox.cs
+++ SequenceLab/Src/BeatBox.cs
@@ -26,22 +26,7 @@ internal class BeatBox : IDisposable
 
     public async Task Run()
     {
-        var sequence = new Sequence
-        {
-            Leader = new Sound("b1")
-            {
-                Strategy = new RepeatStrategy { Count = 32, Interval = 500 },
-                Followers = new() {
-                  new Sound("ts1") {
-                      Strategy = new RepeatStrategy { DelayAfterLeader = 150, Count = 2, Interval = 80 },
-                  },
-                  new Sound("ts2") { Strategy = new PlayOnceStrategy { PlayEveryX = 4 } },
-                  new Sound("ts3") { Strategy = new RepeatStrategy { DelayAfterLeader = 80, Count = 4, Interval = 80, LinearIncrement = -10, PlayEveryX = 8 } },
-                  new Sound("b2") { Strategy = new PlayOnceStrategy { DelayAfterLeader = 250, PlayEveryX = 4 } },
-                  // new Sound("b1") { Strategy = new RepeatStrategy { DelayAfterLeader = 80, Count = 4, Interval = 80, LinearIncrement = -10, PlayEveryX = 16 } },
-               },
-            },
-        };
+        var sequence = Minimal.TechnoBeat1();
 
         var sequenceMessages = sequence.Generate();
         await _channel.Play(sequenceMessages);
---
+++ SequenceLab/Src/Sequences/Minimal.cs
@@ -0,0 +1,24 @@
+﻿namespace Beater;
+
+public class Minimal
+{
+    internal static Sequence TechnoBeat1()
+    {
+        return new Sequence
+        {
+            Leader = new Sound("b1")
+            {
+                Strategy = new RepeatStrategy { Count = 32, Interval = 500 },
+                Followers = new() {
+                  new Sound("ts1") {
+                      Strategy = new RepeatStrategy { DelayAfterLeader = 150, Count = 2, Interval = 80 },
+                  },
+                  new Sound("ts2") { Strategy = new PlayOnceStrategy { PlayEveryX = 4 } },
+                  new Sound("b2") { Strategy = new PlayOnceStrategy { DelayAfterLeader = 250, PlayEveryX = 4 } },
+                  new Sound("ts3") { Strategy = new RepeatStrategy { DelayAfterLeader = 80, Count = 4, Interval = 80, LinearIncrement = -10, PlayEveryX = 8 } },
+                  // new Sound("b1") { Strategy = new RepeatStrategy { DelayAfterLeader = 80, Count = 4, Interval = 80, LinearIncrement = -10, PlayEveryX = 16 } },
+               },
+            },
+        };
+    }
+}
