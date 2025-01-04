a223c13cd6fb7cb1e280681073858b5c205c97ef
yury-kozlov <glassiko@gmail.com>
2025-01-03 23:02:37 -0500

    SoundMessage - remove unused file

--- SequenceLab/Src/SoundMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text;
-
-namespace Beater;
-
-public class SoundMessage
-{
-    public static byte[] Beat1 = Encoding.ASCII.GetBytes("b1 1;");
-    public static byte[] Ts1 = Encoding.ASCII.GetBytes("ts1 1;");
-}
