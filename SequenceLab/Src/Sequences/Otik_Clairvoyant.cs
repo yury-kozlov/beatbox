a7caada3148135dc563782b0b797010bd8c8c4b9
yury-kozlov <glassiko@gmail.com>
2025-03-21 13:42:15 -0400

    Otik Clairvoyant sample

---
+++ SequenceLab/Src/Sequences/Otik_Clairvoyant.cs
@@ -0,0 +1,72 @@
+namespace Beater;
+
+public class Otik_Clairvoyant
+{
+    internal static Sequence GetSequence()
+    {
+
+        var sequence = new Sequence
+        {
+            Leader = new Sound("")
+            {
+                Strategy = new RepeatStrategy() { Count = 4, Interval = 1900, PlayEveryX = 1 },
+                Followers = new()
+            {
+                new Sound("b1")
+                {
+                    Strategy = new PlayOnceStrategy() { PlayEveryX = 1 },
+                    Followers = new ()
+                    {
+                        new Sound("b1")
+                        {
+                            Strategy = new PlayOnceStrategy() { DelayAfterLeader = 360, PlayEveryX = 1 },
+                            Followers = new ()
+                            {
+                                new Sound("b2")
+                                {
+                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 100, PlayEveryX = 1 },
+                                },
+                                new Sound("b1")
+                                {
+                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 340, PlayEveryX = 1 },
+                                    Followers = new ()
+                                    {
+                                        new Sound("b1")
+                                        {
+                                            Strategy = new PlayOnceStrategy() { DelayAfterLeader = 205, PlayEveryX = 1 },
+                                            Followers = new ()
+                                            {
+                                                new Sound("b1")
+                                                {
+                                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 100, PlayEveryX = 1 },
+                                                    Followers = new ()
+                                                    {
+                                                        new Sound("b1")
+                                                        {
+                                                            Strategy = new PlayOnceStrategy() { DelayAfterLeader = 230, PlayEveryX = 1 },
+                                                            Followers = new ()
+                                                            {
+                                                                new Sound("b2")
+                                                                {
+                                                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 100, PlayEveryX = 1 },
+                                                                 }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            }
+
+        };
+
+        return sequence;
+    }
+}
