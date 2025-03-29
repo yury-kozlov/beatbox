f838dda03c8822683dc23517da95118c61d03eb3
yury-kozlov <glassiko@gmail.com>
2025-03-28 23:00:09 -0400

    RepeatStrategy - add ExpandLeaderLoop

--- SequenceLab/Src/Strategy/RepeatStrategy.cs
+++ SequenceLab/Src/Strategy/RepeatStrategy.cs
@@ -7,6 +7,12 @@ public class RepeatStrategy : AbstractStrategy
     public int LinearIncrement;
 
     /// <summary>
+    /// Indicates whether duration of inner loop may exceed duration of the parent loop (making leader's loop longer than it's defined).
+    /// By default false = don't expand inner loops (make them fit parent loop).
+    /// </summary>
+    public bool ExpandLeaderLoop;
+
+    /// <summary>
     /// Will replace current sound with an empty sound preserving the same followers, for example:
     ///  3/4 - each 3rd time out of every 4 will be silenced.
     /// NOTE: this counter is related to every sound within the strategy (not to the strategy itself).
@@ -66,9 +72,19 @@ public class RepeatStrategy : AbstractStrategy
     private SequenceMessage GetEndingMessage(Sound sound)
     {
         var calledTimesText = CheckedTimes == CalledTimes ? $"call #{CheckedTimes}" : $"call #{CalledTimes}, check #{CheckedTimes}";
+
+        var timestamp = DelayAfterLeader + (Interval * Count);
+
+        // check if current loop falls out of leader's loop (e.g. due to incorrect delay of one of the followers which increases total time):
+        if (!ExpandLeaderLoop && sound.Leader?.Strategy is RepeatStrategy leaderLoop && timestamp > leaderLoop.Interval)
+        {
+            // current loop is longer than the leader's loop, make it equal:
+            timestamp = leaderLoop.Interval;
+        }
+
         return new SequenceMessage(null)
         {
-            Timestamp = DelayAfterLeader + (Interval * Count),
+            Timestamp = timestamp,
             Comment = $"{sound.Name} repeat x{Count} ends, {calledTimesText}",
         };
     }
