020608248b6977b245be521b011569159b0bd72c
yury-kozlov <glassiko@gmail.com>
2025-03-29 22:08:10 -0400

    Scsi9_Nebula_Hotel - change to FollowPreviousSoundStrategy

--- SequenceLab/Src/Sequences/Scsi9_Nebula_Hotel.cs
+++ SequenceLab/Src/Sequences/Scsi9_Nebula_Hotel.cs
@@ -1,4 +1,5 @@
 namespace Beater;
+
 /// <summary>
 /// "K  K   S       K   S     K   K   S    K    K  S"
 /// </summary>
@@ -6,95 +7,26 @@ public class Scsi9_Nebula_Hotel
 {
     internal static Sequence GetSequence()
     {
-
         var sequence = new Sequence
         {
             Leader = new Sound("")
             {
-                Strategy = new RepeatStrategy() { Count = 4, Interval = 3940, PlayEveryX = 1 },
+                Strategy = new RepeatStrategy() { Count = 4, Interval = 3940 },
                 Followers = new()
-            {
-                new Sound("b1")
                 {
-                    Strategy = new PlayOnceStrategy() { PlayEveryX = 1 },
-                    Followers = new ()
-                    {
-                        new Sound("b1")
-                        {
-                            Strategy = new PlayOnceStrategy() { DelayAfterLeader = 260, PlayEveryX = 1 },
-                            Followers = new ()
-                            {
-                                new Sound("b2")
-                                {
-                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 230, PlayEveryX = 1 },
-                                    Followers = new ()
-                                    {
-                                        new Sound("b1")
-                                        {
-                                            Strategy = new PlayOnceStrategy() { DelayAfterLeader = 750, PlayEveryX = 1 },
-                                            Followers = new ()
-                                            {
-                                                new Sound("b2")
-                                                {
-                                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 220, PlayEveryX = 1 },
-                                                    Followers = new ()
-                                                    {
-                                                        new Sound("b1")
-                                                        {
-                                                            Strategy = new PlayOnceStrategy() { DelayAfterLeader = 500, PlayEveryX = 1 },
-                                                            Followers = new ()
-                                                            {
-                                                                new Sound("b1")
-                                                                {
-                                                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 250, PlayEveryX = 1 },
-                                                                    Followers = new ()
-                                                                    {
-                                                                        new Sound("b2")
-                                                                        {
-                                                                            Strategy = new PlayOnceStrategy() { DelayAfterLeader = 250, PlayEveryX = 1 },
-                                                                            Followers = new ()
-                                                                            {
-                                                                                new Sound("b1")
-                                                                                {
-                                                                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 400, PlayEveryX = 1 },
-                                                                                    Followers = new ()
-                                                                                    {
-                                                                                        new Sound("b1")
-                                                                                        {
-                                                                                            Strategy = new PlayOnceStrategy() { DelayAfterLeader = 350, PlayEveryX = 1 },
-                                                                                            Followers = new ()
-                                                                                            {
-                                                                                                new Sound("b2")
-                                                                                                {
-                                                                                                    Strategy = new PlayOnceStrategy() { DelayAfterLeader = 250, PlayEveryX = 1 },
-                                                                                                    Followers = new ()
-                                                                                                    {
-
-                                                                                                    }
-                                                                                                }
-                                                                                            }
-                                                                                        }
-                                                                                    }
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    new Sound("b1") { Strategy = new FollowPreviousSoundStrategy() },
+                    new Sound("b1") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 260 } },
+                    new Sound("b2") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 230 } },
+                    new Sound("b1") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 750 } },
+                    new Sound("b2") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 220 } },
+                    new Sound("b1") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 500 } },
+                    new Sound("b1") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 250 } },
+                    new Sound("b2") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 250 } },
+                    new Sound("b1") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 400 } },
+                    new Sound("b1") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 350 } },
+                    new Sound("b2") { Strategy = new FollowPreviousSoundStrategy() { DelayAfterLeader = 250 } },
                 }
             }
-            }
-
         };
 
         return sequence;
