87fe7368f65ef2b4181a54182a7867ed3147cfeb
yury-kozlov <glassiko@gmail.com>
2025-03-29 22:56:24 -0400

    FollowPreviousSoundStrategy - add comment

--- SequenceLab/Src/Strategy/FollowPreviousSoundStrategy.cs
+++ SequenceLab/Src/Strategy/FollowPreviousSoundStrategy.cs
@@ -13,6 +13,7 @@ public class FollowPreviousSoundStrategy : AbstractStrategy
         {
             // make delay relative to the previous sound (instead of being relative to the leader)
             // for example, if leader's timestamp is 1000 and the previous sound is 1200 and delay is 100, the final timestamp will be 1300 (instead of 1100)
+            // note: previous message is considered the last one in the list (if previous messages were generated as part of nested loop with multiple followers, the last one will be taken without any sorting)
             delay += previousMessages.Last().Timestamp;
         }
 
