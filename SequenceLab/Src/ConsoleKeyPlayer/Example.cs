710f26e6bb969c91d1b28b6edb91c756906a5c00
yury-kozlov <glassiko@gmail.com>
2025-04-12 19:55:54 -0400

    console key player - add more examples

--- SequenceLab/Src/ConsoleKeyPlayer/Example.cs
+++ SequenceLab/Src/ConsoleKeyPlayer/Example.cs
@@ -10,9 +10,16 @@ public static partial class Example
         player.Listen();
         player.PrintFormatted();
         player.SaveSequence();
+        player.SaveCode();
         player.PlayRepeated().Wait();
         
         // OR
         player.PlayRepeated("/music/samples/sequences/seq2025-03-01-2114.json").Wait();
+
+        // OR
+        player.PlayRepeated(James_Shinra_Poppin.GetSequence()).Wait();
+
+        // OR
+        player.GenerateCodeFrom("/music/samples/sequences/seq2025-04-05-1719.json");
     }
 }
