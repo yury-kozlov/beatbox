9c572384cd96a8e78780ee073c3dc4f6dda7e082
yury-kozlov <glassiko@gmail.com>
2025-04-12 20:05:06 -0400

    remove comment

--- SequenceLab/Src/BeatBox.cs
+++ SequenceLab/Src/BeatBox.cs
@@ -1,4 +1,4 @@
-namespace Beater;
+﻿namespace Beater;
 
 /// <summary>
 /// based on https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.tcpclient
@@ -39,7 +39,6 @@ internal class BeatBox : IDisposable
         // question: will re-sampling existing tracks into sequences help to learn evolution of sequences using the same sounds ?
         //           it might not help create new sequences but instead will help to repeat existing patterns
 
-        // question: do we need exact time scheduling for each sound or it's ok to have time shifting due to inaccuracy of .net delays?
 
         // TODO 2:   think how to simplify the sequence creation process and make it more readable
         //             e.g.: create strategy that may use another strategy as a follower
