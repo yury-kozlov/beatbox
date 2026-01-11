0a8bee6d5c05ac78359fdbdb3ba51a984d524708
yury-kozlov <glassiko@gmail.com>
2026-01-10 20:09:09 -0500

    move examples to Docs folder

--- SequenceLab/Src/Docs/Examples.cs
+++ SequenceLab/Src/Docs/Examples.cs
@@ -1,30 +1,30 @@
 ﻿namespace Beater;
 
-class FAQ
+class Examples
 {
-    public void ExamplePlay()
+    public void Play()
     {
         // how to play sequence repeatedly
         var player = new ConsoleKeyPlayer();
         player.PlayRepeated(James_Shinra_Poppin.GetSequence()).Wait();
     }
 
-    public void ExampleAppendSequence()
+    public void AppendSequence()
     {
         // how to append one sequence to another
         var result = new Sequence();
-        var slowBeat1 = Minimal.SlowBeat1WithoutRepeats();
-        var slowBeat2 = Minimal.SlowBeat1WithoutRepeats();
-        var slowBeat3 = Minimal.SlowBeat1WithoutRepeats();
+        var sequence1 = Minimal.SlowBeat1WithoutRepeats();
+        var sequence2 = Minimal.SlowBeat1WithoutRepeats();
+        var sequence3 = Minimal.SlowBeat1WithoutRepeats();
         // note: duration property of each sequence should be predefined
-        result.Append(slowBeat1).Append(slowBeat2).Append(slowBeat3);
+        result.Append(sequence1).Append(sequence2).Append(sequence3);
 
         // play
         var player = new ConsoleKeyPlayer();
         player.PlayRepeated(result).Wait();
     }
 
-    public void ExampleInjectSound()
+    public void InjectSound()
     {
         // how to inject sounds into each repetition of a sequence
         var result = new Sequence();
