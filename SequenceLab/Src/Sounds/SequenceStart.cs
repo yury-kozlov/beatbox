89b39d08ed8f3d41875bb8a5c0b0759386a3e111
yury-kozlov <yuryk@thelotter.com>
2026-04-02 20:24:11 -0400

    SequenceStart - add comments

--- SequenceLab/Src/Sounds/SequenceStart.cs
+++ SequenceLab/Src/Sounds/SequenceStart.cs
@@ -10,6 +10,12 @@ public record SequenceStart : NoSound
         Strategy = new FollowPreviousSoundStrategy();
     }
 
+    /// <summary>
+    /// NOTE: We don't store SequenceEnd as part of SequenceDesign 
+    /// (but rather search for it dynamically by sequence-start) because when generating actual sequence
+    /// each sound is cloned with updated timestamp. In repeated sequences it will be impossible to always work 
+    /// on the same instance of a SequenceEnd sound shared between different iterations.
+    /// </summary>
     public SequenceEnd GetSequenceEnd() => (SequenceEnd)Followers.Last(s => s is SequenceEnd);
 
     public override string? ToString() => Format(FriendlyName);
